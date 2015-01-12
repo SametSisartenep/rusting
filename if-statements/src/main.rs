6dab74d67559f41955b3ba35e9b138eeb76f2182
SametSisartenep <krashbuffer@gmail.com>
2015-01-12 19:31:37 +0100

    'if' statements are very cool in Rust :P

--- if-statements/src/main.rs
+++ if-statements/src/main.rs
@@ -1,5 +1,20 @@
-let x = 5;
+fn main() {
+  let (x, y) = (5, 3);
 
-if x == 5 {
-  println!("x is five!");
+  ///////////////
+  if x == 5 {
+    println!("x is five!");
+  }
+
+  ///////////////
+  if y == 4 {
+    println!("y is four!");
+  } else {
+    println!("y is not four :(");
+  }
+
+  ///////////////
+  let y = if x == 5 { 10 } else { 15 };
+
+  println!("The final value of y is: {}", y);
 }
